81b0caba4ee801584cbbf31dc91cc57c272fe3dd
DianaUzun <dianauzun2006@gmail.com>
2023-04-19 20:32:14 +0300

    add messages, change sign up template

--- ToDo/lists/views.py
+++ ToDo/lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.contrib import messages
 
 
 def index(request):
@@ -14,20 +15,26 @@ def get_lists(request):
 
 
 def list_delete(request, id):
-    list = List.objects.get(pk=id)
-    list.delete()
+    try:
+        list = List.objects.get(pk=id)
+        list.delete()
+    except:
+        messages.error(request, 'Lose to delete this list!')
+    else:
+        messages.success(request, 'ToDo List was successfully deleted!')
     return redirect('lists')
 
 
 def list_create(request):
     if request.method == 'POST':
-        print(request.POST)
+        #print(request.POST)
         form = ListForm(request.POST, initial={'user': request.user.id})
         if form.is_valid():
             form.save()
+            messages.success(request, 'ToDo List was successfully created!')
             return redirect('lists')
         else:
-            print('Error')
+            messages.error(request, form.errors)
     else:
         form = ListForm(initial={'user': request.user.id})
     return render(request, 'lists/list_create.html', {'form': form})
--- ToDo/myauth/views.py
+++ ToDo/myauth/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import *
 
 
 def create_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserAddForm(request.POST)
         if form.is_valid():
             # только регистрация
             form.save()
+            messages.success(request, 'You are successfully create account!')
             return redirect('signin')
 
             # регистрация и вход
             # user = form.save()
             # login(request, user)
+            # messages.success(request, 'You are successfully create your account! You are logged in')
             # return redirect('home')
         else:
-            print(form.errors)
+            messages.error(request, form.errors)
     else:
-        form = UserCreationForm()
+        form = UserAddForm()
     return render(request, 'myauth/create_user.html', {'form': form})
 
 
@@ -27,18 +30,18 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        print(username, password)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            print('okey')
+            messages.success(request, 'You are logged in')
             return redirect('lists')
         else:
-            print('Error')
+            messages.error(request, 'Error happened! Please write correct data!')
     return render(request, 'myauth/signin.html')
 
 
 def signout(request):
     logout(request)
+    messages.success(request, 'You are logged out')
     return redirect('home')
 
